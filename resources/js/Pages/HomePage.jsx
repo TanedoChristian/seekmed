c5ad35c73ec3caadaad56b01e59d462bb9bb7f13
Chirstian <tanedochrisjohn@gmail.com>
2024-10-27 22:55:22 +0800

    latest

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 export default function HomePage() {
     return (
         <div>
@@ -37,7 +39,38 @@ export default function HomePage() {
                                 <button
                                     class="bg-main text-white rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3 px-6 text-sm lg:ml-5 hover:bg-indigo-700"
                                     onClick={() => {
-                                        window.location.href = "/register";
+                                        Swal.fire({
+                                            title: "Select your role",
+                                            input: "select",
+                                            inputOptions: {
+                                                user: "User",
+                                                rider: "Rider",
+                                            },
+                                            inputPlaceholder: "Select a role",
+                                            showCancelButton: true,
+                                            confirmButtonText: "Confirm",
+                                        }).then((result) => {
+                                            if (result.isConfirmed) {
+                                                console.log(
+                                                    "You selected:",
+                                                    result.value
+                                                );
+
+                                                if (result.value == "user") {
+                                                    window.location.href =
+                                                        "/register";
+                                                } else if (
+                                                    result.value == "rider"
+                                                ) {
+                                                    window.location.href =
+                                                        "/register/rider";
+                                                } else {
+                                                    Swal.fire({
+                                                        title: "Choose One User",
+                                                    });
+                                                }
+                                            }
+                                        });
                                     }}
                                 >
                                     Sign up
